909f4430643ecf1acfadddb7d4c638fcadfbe890
KaterynaSiniuta <icewater17@gmail.com>
2024-10-25 14:36:15 +0300

    2_1

---
+++ 2_1.py
@@ -0,0 +1,11 @@
+fournum = int(input("Введіть 4-х значне число: "))
+
+tysyacha = fournum // 1000
+sotnya = (fournum // 100) % 10
+desyatok = (fournum // 10) % 10
+odyn = fournum % 10
+
+print(tysyacha)
+print(sotnya)
+print(desyatok)
+print(odyn)
