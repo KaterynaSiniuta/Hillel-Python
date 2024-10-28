a76db23082a91e4cffa71e7138d1824a13bc7935
KaterynaSiniuta <icewater17@gmail.com>
2024-10-28 22:10:23 +0200

    calculator

--- 3_1.py
+++ 3_1.py
@@ -1,1 +1,17 @@
-6
+number1 = float(input("Введіть перше число: "))
+operation = input("Введіть операцію (+, -, *, /): ")
+number2 = float(input("Введіть друге число: "))
+
+if operation == '+':
+    print("Результат: ", number1 + number2)
+elif operation == '-':
+    print("Результат: ", number1 - number2)
+elif operation == '*':
+    print("Результат: ", number1 * number2)
+elif operation == '/':
+    if number2 == 0:
+        print("Помилка: Дільник не може дорівнювати нулю")
+    else:
+        print("Результат: ", number1 / number2)
+else:
+    print("Невідома операція")
