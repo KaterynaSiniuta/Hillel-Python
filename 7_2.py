74c46065a228e1f89656e43e4ee2092b9ec89949
KaterynaSiniuta <icewater17@gmail.com>
2024-11-19 06:35:25 +0200

    7_2

---
+++ 7_2.py
@@ -0,0 +1,24 @@
+def correct_sentence(text):
+    sentences = text.split('.')
+    corrected_sentences = []
+
+    for sentence in sentences:
+        sentence = sentence.strip()
+        if sentence:
+            sentence = sentence.capitalize()
+            if sentence[-1] != '.':
+                sentence += '.'
+            corrected_sentences.append(sentence)
+
+    return ' '.join(corrected_sentences)
+
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert (correct_sentence("Greetings, friends.")
+        == "Greetings, friends."), 'Test4'
+assert (correct_sentence("greetings, friends.")
+        == "Greetings, friends."), 'Test5'
+
+print('ОК')
