6dae0afc6804ef6943b5e30b4748d2e17ec2d847
KaterynaSiniuta <icewater17@gmail.com>
2024-11-19 14:53:02 +0200

    7_4

--- 7_4.py
+++ 7_4.py
@@ -2,7 +2,7 @@ def common_elements():
     numbers_divisible_by_3 = {x for x in range(100) if x % 3 == 0}
     numbers_divisible_by_5 = {x for x in range(100) if x % 5 == 0}
 
-    return numbers_divisible_by_3 & numbers_divisible_by_5  p
+    return numbers_divisible_by_3 & numbers_divisible_by_5
 
 
 # Тест
