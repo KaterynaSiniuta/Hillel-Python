bd06b2e2aa1c0955fc4617bc9d6623aa531bba61
KaterynaSiniuta <icewater17@gmail.com>
2024-11-19 06:46:32 +0200

    7_4

---
+++ 7_4.py
@@ -0,0 +1,11 @@
+def common_elements():
+    numbers_divisible_by_3 = {x for x in range(100) if x % 3 == 0}
+    numbers_divisible_by_5 = {x for x in range(100) if x % 5 == 0}
+
+    return numbers_divisible_by_3 & numbers_divisible_by_5  p
+
+
+# Тест
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+
+print('ОК')
