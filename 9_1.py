84d5b485e9568aaad0dad1776ce992812fcb7593
KaterynaSiniuta <icewater17@gmail.com>
2024-11-22 18:19:37 +0200

    9_1

--- 9_1.py
+++ 9_1.py
@@ -10,16 +10,14 @@ def popular_words(text: str, words: list) -> dict:
     dict: A dictionary where the keys are the words from the `words` list,
           and the values are the number of times each word appears in the text.
     """
-    # Convert the text to lowercase and split it into words
+
     word_list = text.lower().split()
 
-    # Create a dictionary of results
     result = {word: word_list.count(word) for word in words}
 
     return result
 
 
-# Function test
 assert popular_words(
     '''When I was One I had just begun When I was Two I was nearly new''',
     ['i', 'was', 'three', 'near']
